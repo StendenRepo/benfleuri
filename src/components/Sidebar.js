f583578c71f04a0ca91ffe497cca594585e4c0f6
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-12-01 13:32:40 +0100

    made side bar front end

---
+++ src/components/Sidebar.js
@@ -0,0 +1,56 @@
+import styles from "../styles/Sidebar.module.css"
+import Image from 'next/image'
+
+export default function sidebar() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.imagecontainer}>
+                <Image
+                    src="/Logo.svg"
+                    width={100}
+                    height={100}
+                    className={styles.logo}
+                />
+            </div>
+            <div>
+                <p className={styles.welcomeText}>Goedemorgen, Bernhardus</p>
+                <p className={styles.admin}>Admin</p>
+            </div>
+            <hr className={styles.hr}></hr>
+            <div className={styles.pages}>
+                <div className={styles.screenBox}>
+                    <Image
+                        src='/heart.svg'
+                        width={25}
+                        height={25}
+                    />
+                    <p className={styles.screenName}>Dashboard</p>
+                </div>
+                <div className={styles.screenBox}>
+                    <Image
+                        src='/heart.svg'
+                        width={25}
+                        height={25}
+                    />
+                    <p className={styles.screenName}>Nieuwe Bestelling</p>
+                </div>
+                <div className={styles.screenBox}>
+                    <Image
+                        src='/heart.svg'
+                        width={25}
+                        height={25}
+                    />
+                    <p className={styles.screenName}>Bestellingen</p>
+                </div>
+                <div className={styles.screenBox}>
+                    <Image
+                        src='/heart.svg'
+                        width={25}
+                        height={25}
+                    />
+                    <p className={styles.screenName}>Gebruikerbeheer</p>
+                </div>
+            </div>
+        </div>
+    )
+}
