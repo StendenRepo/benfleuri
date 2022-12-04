d268fb05ec8d2d9d5cb8c06e28bce342ea8c3456
Monique <89770769+Moniquesbg@users.noreply.github.com>
2022-12-04 16:40:51 +0100

    Lay-out forms besteller en ontvanger gemaakt. en header van de pagina

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from '../styles/viewOrder.module.css'
 
-export default function Home() {
+export default function viewOrder() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -12,6 +12,131 @@ export default function Home() {
           <h1 className={styles.h1}>Bestelling 1</h1>
           <button className = {styles.exportButton} type="button">Exporteer bestellingen</button>
         </div>
+      </div>
+
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <p className={styles.cardTitle}>Bestelling gegevens</p>
+        </div>
+
+
+        <div className={styles.orderData}>
+          <div className={styles.bestellerSection}>
+            <h3>Besteller</h3>
+            <form action="" method="POST">
+              <div className={styles.formRow}>
+                <div className={styles.labelInput}>
+                  <label for="naamOpdrachtgever">Naam opdrachtgever</label>
+                  <input className={styles.input} type="text" name="naamOpdrachtgever" id="naamOpdrachtgever"></input>
+                </div>
+                <div className={styles.labelInput}>
+                  <label for="naamContactpersoon">Naam contactpersoon</label>
+                  <input className={styles.input} type="text" name="naamContactpersoon" id="naamContactpersoon"></input>
+                </div>
+              </div>
+              <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="straatnaam">Straatnaam</label>
+                    <input className={styles.input} type="text" name="straatnaam" id="straatnaam"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="nummer">nummer</label>
+                    <input className={styles.input} type="text" name="nummer" id="nummer"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="postcode">Postcode</label>
+                    <input className={styles.input} type="text" name="postcode" id="postcode"></input>
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="plaatsnaam">plaatsnaam</label>
+                    <input className={styles.input} type="text" name="plaatsnaam" id="plaatsnaam"></input>
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="telefoonnummer">Telefoonnummer</label>
+                    <input className={styles.input} type="text" name="telefoonnummer" id="telefoonnummer"></input>
+                  </div>
+                </div>
+                <div className={styles.formRowRadio}>
+                  <label>Bezorgkosten</label>
+                   <div className={styles.inputRadio}>
+                      <input className={styles.input} type="radio" name="bezorgkosten" id="ja" value="ja"></input>
+                      <label for="ja">ja</label>
+                      <input className={styles.input} type="radio" name="bezorgkosten" id="nee" value="nee"></input>
+                      <label for="nee">nee</label>
+                   </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="datumBezorging">datum van bezorging</label>
+                    <input className={styles.input} type="date" name="datumBezorging" id="datumBezorging"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="verzending">Type verzending</label>
+                    <select name="verzending" id="verzending">
+                      <option value="bezorging">Bezorging</option>
+                      <option value="afhalen">Afhalen</option>
+                    </select>
+                  </div>
+                </div>
+            </form>
+          </div>
+
+          <div className={styles.ontvangerSection}>
+            <h3>Ontvanger</h3>
+              <form action="" method="POST">
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="naamOpdrachtgever">Naam opdrachtgever</label>
+                    <input className={styles.input} type="text" name="naamOpdrachtgever" id="naamOpdrachtgever"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="naamContactpersoon">Naam contactpersoon</label>
+                    <input className={styles.input} type="text" name="naamContactpersoon" id="naamContactpersoon"></input>
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="straatnaam">Straatnaam</label>
+                    <input className={styles.input} type="text" name="straatnaam" id="straatnaam"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="nummer">nummer</label>
+                    <input className={styles.input} type="text" name="nummer" id="nummer"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="postcode">Postcode</label>
+                    <input className={styles.input} type="text" name="postcode" id="postcode"></input>
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="plaatsnaam">plaatsnaam</label>
+                    <input className={styles.input} type="text" name="plaatsnaam" id="plaatsnaam"></input>
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="telefoonnummer">Telefoonnummer</label>
+                    <input className={styles.input} type="text" name="telefoonnummer" id="telefoonnummer"></input>
+                  </div>
+                </div>
+                <div className={styles.formRow}>
+                  <div className={styles.labelInput}>
+                    <label for="prijs">Prijs bestelling</label>
+                    <input className={styles.input} type="text" name="prijs" id="prijs"></input>
+                  </div>
+                  <div className={styles.labelInput}>
+                    <label for="prijsTotaal">Prijs totaal</label>
+                    <input className={styles.input} type="text" name="prijsTotaal" id="prijsTotaal"></input>
+                  </div>
+                </div>
+              </form>
+          </div>
+        </div> 
       </div>
     </div>
   )
