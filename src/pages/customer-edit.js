5f10fbb7e710d1f3c948c65beb6922391f63e226
rickv21 <rick.vinke@student.nhlstenden.com>
2022-12-07 15:48:21 +0100

    - Most of the EditCustomer page is now using React objects and Tailwind styling.

--- src/pages/customer-edit.js
+++ src/pages/customer-edit.js
@@ -1,77 +1,93 @@
 import MainLayout from '../layout/MainLayout';
 import styles from '../styles/EditCustomer.module.css'
 
+function Header() {
+    return <div className={`font-['Roboto'] ml-[5%] mt-[2%]`} >
+        <div className={`font-['Roboto'] ml-[2%] mb-[1%]`}>
+            <a href={"#"}>Klantoverzicht</a>
+        </div>
+        <div className={`font-['Roboto'] text-2xl font-bold`}>
+            Klanten
+        </div>
+    </div>
+}
+
+function FormRow({children}){
+    return <div className={`flex gap-x-[10px] justify-between`}>
+        {children}
+    </div>
+}
+
+function InputField(props) {
+    let type = props.type === undefined ? "text" : props.type;
+    let labelStyle = "block text-[#39383A] text-xs font-['Roboto'] ";
+    let inputStyle = "block text-black bg-white h-[25px] " +
+        "w-full border-1 border-[#ADADAE] font-['Roboto'] ";
+
+    if (props.name === "house-number") {
+        labelStyle += "w-[10%]"
+    } else if (props.name === "postal-code") {
+        labelStyle += "w-[18%] ml-auto"
+    } else {
+        if (props.size === "full") {
+            labelStyle += "flex-grow"
+        } else if (props.size === "half") {
+            labelStyle += "w-[49%]"
+        }
+    }
+    return <label className={`${labelStyle}`}>
+        {props.label}
+        <input name={props.name} className={`${inputStyle}`} type={type}/>
+    </label>
+}
+
+function Buttons() {
+    return <div className={`flex gap-x-[10px] flex-row justify-end`}>
+        <button className={`text-sm py-[8px] px-[20px] font-['Roboto'] 
+        bg-[#00A952] text-white font-bold border-0 rounded`}
+                type="submit">Wijzig klant</button>
+        <a href={"#"}>
+            <button className={`text-sm font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
+         font-['Roboto'] bg-white text-black`} type="button">Annuleren</button>
+        </a>
+    </div>
+}
+
 export default function EditCustomerPage() {
     return (
         <MainLayout>
-            <div className={styles.header}>
-                <div className={styles.navigation}>
-                    <a href={"#"}>Klantoverzicht</a>
-                </div>
-                <div className={styles.title}>
-                    Klanten
-                </div>
-            </div>
-        <div className={styles.container}>
-
-            <div className={styles.topBlock}>
-                <div>
-                    <p>Klantgegevens</p>
-                </div>
-            </div>
-            <div className={styles.block}>
-            <form action='' method='post' className={styles.blockForm}>
-                <div className={styles.formInputs}></div>
-                    <div className={styles.formRow}>
-                        <label className={ `${styles.inputLabel} ${styles.inputLabelFullWidth}` }>
-                            Naam opdrachtgever (optioneel)
-                            <input name="client-name" className={ `${styles.inputName} ${styles.inputField}` } type="text" />
-                        </label>
+            <Header/>
+            <div className={styles.container}>
+                <div className={styles.topBlock}>
+                    <div>
+                        <p>Klantgegevens</p>
                     </div>
-                <div className={styles.formRow}>
-                    <label className={ `${styles.inputLabel} ${styles.inputLabelHalfWidth}` }>
-                        Naam
-                        <input name="customer-firstname" className={ `${styles.inputName} ${styles.inputField}` } type="text" />
-                    </label>
-                    <label className={ `${styles.inputLabel} ${styles.inputLabelHalfWidth}` }>
-                        Achternaam
-                        <input name="customer-lastname" className={ `${styles.inputName} ${styles.inputField}` } type="text" />
-                    </label>
                 </div>
-                    <div className={styles.formRow}>
-                        <label className={ `${styles.inputLabel} ${styles.inputLabelHalfWidth}` }>
-                            Straatnaam
-                            <input name="address" className={ `${styles.inputName} ${styles.inputField}` } type="text" />
-                        </label>
-                        <label className={ `${styles.inputLabel} ${styles.inputNumber}` }>
-                            Nummer
-                            <input name="house-number" className={ `${styles.inputField}` } type="text" />
-                        </label>
-                        <label className={ `${styles.inputLabel} ${styles.inputPostal}` }>
-                            Postcode
-                            <input name="postal_code" className={ `${styles.inputField}` } type="text" />
-                        </label>
-                    </div>
-                <div className={styles.formRow}>
-                    <label className={ `${styles.inputLabel} ${styles.inputLabelFullWidth}` }>
-                        Plaats
-                        <input name="residence" className={ `${styles.inputName} ${styles.inputField}` } type="text" />
-                    </label>
+                <div className={styles.block}>
+                    <form action='' method='post' className={styles.blockForm}>
+                        <div className={styles.formInputs}></div>
+                        <FormRow>
+                            <InputField name="client-name" size="full" label="Naam opdrachtgever (optioneel)"/>
+                        </FormRow>
+                        <FormRow>
+                            <InputField name="customer-firstname" size="half" label="Voornaam"/>
+                            <InputField name="customer-lastname" size="half" label="Achternaam"/>
+                        </FormRow>
+                        <FormRow>
+                            <InputField name="address" size="half" label="Straatnaam"/>
+                            <InputField name="house-number" label="Nummer"/>
+                            <InputField name="postal-code" label="Postcode"/>
+                        </FormRow>
+                        <FormRow>
+                            <InputField name="residence" size="full" label="Plaats"/>
+                        </FormRow>
+                        <FormRow>
+                            <InputField name="phone" size="full" type="tel" label="Telefoon Nr"/>
+                        </FormRow>
+                        <Buttons/>
+                    </form>
                 </div>
-                <div className={styles.formRow}>
-                    <label className={ `${styles.inputLabel} ${styles.inputLabelFullWidth}` }>
-                        Telefoon Nr
-                        <input name="phone" className={ `${styles.inputName} ${styles.inputField}` } type="tel" />
-                    </label>
-                </div>
-
-                <div className={styles.formButtons}>
-                    <button className={styles.submitButton} type="submit">Wijzig klant</button>
-                    <a href={"#"}><button className={styles.cancelButton} type="button">Annuleren</button></a>
-                </div>
-            </form>
             </div>
-        </div>
         </MainLayout>
     )
 }
