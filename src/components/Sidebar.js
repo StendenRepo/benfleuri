1bcca2ada434ced75db7ed26f23b1f867de3c4ca
DelanoKoster1 <delano.koster@student.nhlstenden.com>
2022-12-07 12:59:50 +0100

    began merging to tailwind

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,75 +3,79 @@ import Image from 'next/image';
 
 export default function Sidebar() {
   return (
-    <div className="z-1 bg-white w-60 h-screen shadow-lg shadow-black ">
+    <div className="z-1 bg-white w-72 h-screen shadow-lg shadow-black ">
       <div className="flex justify-center">
         <Image
           src="/Logo.svg"
           width={100}
           height={100}
-          className={styles.logo}
+          className="mt-8"
           alt="Benfleuri-icon"
         />
       </div>
       <div>
-        <p className={styles.welcomeText}>Goedemorgen, Bernhardus</p>
-        <p className={styles.admin}>Admin</p>
+        <p className="text-base text-black mt-6 ml-3">
+          Goedemorgen, Bernhardus
+        </p>
+        <p className="text-sm text-gray-500 ml-3 mb-3">Admin</p>
       </div>
-      <hr className={styles.hr}></hr>
-      <div className={styles.pages}>
-        <div className={styles.screenBox}>
+      <hr></hr>
+      <div className="flex flex-col">
+        <div className="flex align-middle pl-3 pt-3">
           <Image
             src="/heart.svg"
             width={20}
             height={20}
-            className={styles.heart}
+            className="grayscale"
             alt="Icon"
           />
-          <p className={styles.screenName}>Dashboard</p>
+          <p className="text-black text-base pb-1 pl-9">Dashboard</p>
         </div>
-        <div className={styles.screenBox}>
+        <div className="flex content-center pl-3">
           <Image
             src="/heart.svg"
             width={20}
             height={20}
-            className={styles.heart}
+            className="grayscale"
             alt="Icon"
           />
-          <p className={styles.screenName}>Nieuwe Bestelling</p>
+          <p className="text-black text-base pb-1 pl-9">Nieuwe Bestelling</p>
         </div>
-        <div className={styles.screenBox}>
+        <div className="flex content-center pl-3">
           <Image
             src="/heart.svg"
             width={20}
             height={20}
-            className={styles.heart}
+            className="grayscale"
             alt="Icon"
           />
-          <p className={styles.screenName}>Bestellingen</p>
+          <p className="text-black text-base pb-1 pl-9">Bestellingen</p>
         </div>
-        <div className={styles.screenBox}>
+        <div className="flex content-center pl-3">
           <Image
             src="/heart.svg"
             width={20}
             height={20}
-            className={styles.heart}
+            className="grayscale"
             alt="Icon"
           />
-          <p className={styles.screenName}>Gebruikerbeheer</p>
+          <p className="text-black text-base pb-1 pl-9">Gebruikerbeheer</p>
         </div>
-        <div className={styles.screenBox}>
+        <div className="flex content-center pl-3">
           <Image
             src="/heart.svg"
             width={20}
             height={20}
-            className={styles.heart}
+            className="grayscale"
             alt="Icon"
           />
-          <p className={styles.screenName}>Klanten</p>
+          <p className="text-black text-base pb-1 pl-9">Klanten</p>
         </div>
       </div>
-      <div className={styles.logoutContainer}>
-        <button className={styles.logoutButton}>Logout</button>
+      <div className="flex justify-center content-center">
+        <button className="bg-primary-green rounded-3xl w-30 h-6 text-white">
+          Logout
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#009A40',
+        'hover-green': '#009a40be',
+      },
+    },
   },
   plugins: [],
 };
