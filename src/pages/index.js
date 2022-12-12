987f8e5d98810e0fc96a421970f0f1a80a7d0db6
Hajo300 <89904094+Hajo300@users.noreply.github.com>
2022-12-12 10:21:02 +0100

    added change user page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import MainLayout from '../layout/mainLayout';
 import CreateUser from './admin/createUser/createUser';
+import ChangeUser from './admin/changeUser/changeUser';
 
 export default function Home() {
   return (
