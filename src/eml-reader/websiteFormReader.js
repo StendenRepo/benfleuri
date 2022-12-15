ccd9bdc728982ada54bc7919bd855e390dda0bc8
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-12-15 14:11:03 +0100

    can now extract reciever info from bestelformulier

--- src/eml-reader/websiteFormReader.js
+++ src/eml-reader/websiteFormReader.js
@@ -18,5 +18,16 @@ mailparser.simpleParser(emlFile, (error, parsedMail) => {
     const from = parsedMail.from['value'][0]['name']
     const orderDate = parsedMail.date
     const subject = parsedMail.subject
+
+
+    const deliveryDate = parsedMail.text.match(/([0-9]+(\/[0-9]+)+)/i)[0]
+    const reciever = filteredMailLines[filteredMailLines.indexOf('gegevens ontvanger') + 1]
+
+
+    const deliveryAdress = filteredMailLines[filteredMailLines.indexOf('bezorgingsadres') + 1]
+    const deliveryPostalCode = filteredMailLines[filteredMailLines.indexOf('bezorgingsadres') + 2].replace(" ", "").substring(0, 7)
+    const city = filteredMailLines[filteredMailLines.indexOf('bezorgingsadres') + 2].replace(" ", "").substring(7)
+    const description = filteredMailLines[filteredMailLines.lastIndexOf('gegevens bestelling') + 1]
+
 })
 
