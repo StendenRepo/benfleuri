d5c59206d6bd815886afc00ae22317efe065e86e
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-12-15 14:08:26 +0100

    can now extract email, name, orderDate and subject from bestelformulier

--- src/eml-reader/websiteFormReader.js
+++ src/eml-reader/websiteFormReader.js
@@ -8,4 +8,15 @@ mailparser.simpleParser(emlFile, (error, parsedMail) => {
         console.log(error)
         return
     }
+
+
+    const splitUpMailLines = parsedMail.text.split('\n').filter(element => element != "")
+
+    const filteredMailLines = splitUpMailLines.map(element => element.toLowerCase())
+
+    const fromEmail = parsedMail.from['value'][0]['adress']
+    const from = parsedMail.from['value'][0]['name']
+    const orderDate = parsedMail.date
+    const subject = parsedMail.subject
 })
+
