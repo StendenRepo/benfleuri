a426ddfc45b56d6122d24ddd75a473849fbf5320
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-12-16 11:17:22 +0100

    made some small adjustments

--- src/eml-reader/websiteFormReader.js
+++ src/eml-reader/websiteFormReader.js
@@ -1,67 +1,65 @@
 const fs = require('fs')
 const mailparser = require('mailparser')
 
-const emlFile = fs.readFileSync('sample.eml', 'utf8')
 
-mailparser.simpleParser(emlFile, (error, parsedMail) => {
-    if(error) {
-        console.log(error)
-        return
-    }
+function extractOrderFormData() {
+  const emlFile = fs.readFileSync('sample.eml', 'utf8')
 
+  mailparser.simpleParser(emlFile, (error, parsedMail) => {
+      if(error) {
+          console.log(error)
+          return
+      }
 
-  // splits at every new line of the mail, putting the data in an array it then filters all of the empty entries out of the array
-    const splitUpMailLines = parsedMail.text.split('\n').filter(element => element != "")
+    // splits at every new line of the mail, putting the data in an array it then filters all of the empty entries out of the array
+      const filteredMailLines = parsedMail.text.split('\n').filter(element => element != "")
 
-    const filteredMailLines = splitUpMailLines.map(element => element.toLowerCase())
+    // gets the standard email header data
+      const fromEmail = parsedMail.from['value'][0]['address']
+      const from = parsedMail.from['value'][0]['name']
+      const orderDate = parsedMail.date
+      const subject = parsedMail.subject
 
-  // gets the standard email header data
-    const fromEmail = parsedMail.from['value'][0]['address']
-    const from = parsedMail.from['value'][0]['name']
-    const orderDate = parsedMail.date
-    const subject = parsedMail.subject
+    // uses regex to extract the deliveryDate from the text
+      const deliveryDate = parsedMail.text.match(/([0-9]+(\/[0-9]+)+)/i)[0]
+      const reciever = filteredMailLines[filteredMailLines.indexOf('GEGEVENS ONTVANGER') + 1]
 
-  // uses regex to extract the deliveryDate from the text
-    const deliveryDate = parsedMail.text.match(/([0-9]+(\/[0-9]+)+)/i)[0]
-    const reciever = filteredMailLines[filteredMailLines.indexOf('gegevens ontvanger') + 1]
+    // gets adress data by looking for specific text like 'bezorgingsadres', and returns the array element after it
+      const deliveryAdress = filteredMailLines[filteredMailLines.indexOf('BEZORGINGSADRES') + 1]
+      const deliveryPostalCode = filteredMailLines[filteredMailLines.indexOf('BEZORGINGSADRES') + 2].replace(" ", "").substring(0, 7)
+      const city = filteredMailLines[filteredMailLines.indexOf('BEZORGINGSADRES') + 2].replace(" ", "").substring(7)
+      const description = filteredMailLines[filteredMailLines.lastIndexOf('GEGEVENS BESTELLING') + 1]
 
-  // gets adress data by looking for specific text like 'bezorgingsadres', and returns the array element after it
-    const deliveryAdress = filteredMailLines[filteredMailLines.indexOf('bezorgingsadres') + 1]
-    const deliveryPostalCode = filteredMailLines[filteredMailLines.indexOf('bezorgingsadres') + 2].replace(" ", "").substring(0, 7)
-    const city = filteredMailLines[filteredMailLines.indexOf('bezorgingsadres') + 2].replace(" ", "").substring(7)
-    const description = filteredMailLines[filteredMailLines.lastIndexOf('gegevens bestelling') + 1]
+    // first finds the element that contains the word 'bedrag', then splits it into two at the : and replaces unnecessary characters
+      const price = filteredMailLines.find(word => word.includes('Bedrag')).split(':')[1].replaceAll(" ", "").replace("€", "")
 
-  // first finds the element that contains the word 'bedrag', then splits it into two at the : and replaces unnecessary characters
-    const price = filteredMailLines.find(word => word.includes('bedrag')).split(':')[1].replaceAll(" ", "").replace("€", "")
+      const findWithCard = filteredMailLines.find(word => word.includes('Kaartje toevoegen:')).split(':')[1].replace(" ", "")
+      const withCard = findWithCard == 'ja' ? true : false
 
-    const findWithCard = filteredMailLines.find(word => word.includes('kaartje toevoegen:')).split(':')[1].replace(" ", "")
-    const withCard = findWithCard == 'ja' ? true : false
+      const cardText = filteredMailLines.find(word => word.includes('Tekst kaartje:')).split(':')[1]
 
-    const cardText = filteredMailLines.find(word => word.includes('tekst kaartje:')).split(':')[1]
+    //finds index of the element where the aanvullingen/opmerkingen beginnen
+      const commentsIndex = filteredMailLines.indexOf(filteredMailLines.find(word => word.includes('AANVULLINGEN')))
+      const comments = filteredMailLines.slice(commentsIndex + 1, -1).join(' ') // Joins all comments to a string
 
-  //finds index of the element where the aanvullingen/opmerkingen beginnen
-    const commentsIndex = filteredMailLines.indexOf(filteredMailLines.find(word => word.includes('aanvullingen')))
+      const extractedData = {
+          'from': from,
+          'fromEmail': fromEmail,
+          'orderDate': orderDate,
+          'subject': subject,
+          'deliveryDate': deliveryDate,
+          'reciever': reciever,
+          'deliveryAdress': deliveryAdress,
+          'deliveryPostalCode': deliveryPostalCode,
+          'city': city,
+          'description': description,
+          'price': price,
+          'withCard': withCard,
+          'cardText': cardText,
+          'comments': comments
+      }
 
-  // takes all elements that belong to aanvullingen/opmerkingen and joins them to a string
-    const comments = filteredMailLines.slice(commentsIndex + 1, -1).join(' ')
-
-    const extractedData = {
-        'from': from,
-        'fromEmail': fromEmail,
-        'orderDate': orderDate,
-        'subject': subject,
-        'deliveryDate': deliveryDate,
-        'reciever': reciever,
-        'deliveryAdress': deliveryAdress,
-        'deliveryPostalCode': deliveryPostalCode,
-        'city': city,
-        'description': description,
-        'price': price,
-        'withCard': withCard,
-        'cardText': cardText,
-        'comments': comments
-    }
-
-    console.log(extractedData)
-})
-
+      console.log(extractedData)
+      return extractedData
+  })
+}
