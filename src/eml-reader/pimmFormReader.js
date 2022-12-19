fb25b1e99fe8e5c45c82dc89f54b95ca37ce048d
Jessev3 <jesse.vugteveen@student.nhlstenden.com>
2022-12-19 17:05:16 +0100

    added all data to object

--- src/eml-reader/pimmFormReader.js
+++ src/eml-reader/pimmFormReader.js
@@ -3,7 +3,7 @@ const mailparser = require('mailparser')
 
 
 function extractPimmSolutionsFormData() {
-  const emlFile = fs.readFileSync('sample3.eml', 'utf8')
+  const emlFile = fs.readFileSync('sample2.eml', 'utf8')
 
   mailparser.simpleParser(emlFile, (error, parsedMail) => {
       if(error) {
@@ -11,9 +11,10 @@ function extractPimmSolutionsFormData() {
           return
       }
 
-    // splits at every new line of the mail, putting the data in an array it then filters all of the empty entries out of the array
+      // splits at every new line of the mail, putting the data in an array it then filters all unnecesary data out of the array
       const filteredMailLines = parsedMail.text.split('\n').filter(element => element != "" && !element.includes("https") && !element.includes("Klik voor"))
 
+      // counts the amount of table rows so that that information can be used later on
       var tableRowCount = 0;
       filteredMailLines.forEach(element => {
           if(element == "1 x") {
@@ -21,23 +22,28 @@ function extractPimmSolutionsFormData() {
           }
       });
       
-    // gets the standard email header data
+      // gets the standard email header data
       const fromEmail = parsedMail.from['value'][0]['address']
       const from = parsedMail.from['value'][0]['name']
       const orderDate = parsedMail.date
       const subject = parsedMail.subject
+
+      // Gets the delivery date by searching for "afleverdatum", then splits at the semicolon and extracts the date from the remaining part
+      const deliveryDate = filteredMailLines.find(word => word.includes("afleverdatum:")).split(":")[1].match(/([0-9]+(-[0-9]+)+)/i)[0]
 
       const companyName = filteredMailLines[1]
       const name = filteredMailLines[2]
 
       const adress = filteredMailLines[3]
 
+      // City and postalcode are in the same array element, so it is split in two parts and city gets popped from the array. The remaining part is the postalCode
       const postalCodeAndCity = filteredMailLines[4].split(/\s+/)
       const city = postalCodeAndCity.pop()
       const postalCode = postalCodeAndCity.join(" ").replace(" ", "")
 
       const telNumber = filteredMailLines[5].trim()
     
+      // The order description follows after the first occurance of "1 x". 
       const descriptionIndex = filteredMailLines.indexOf("1 x")
       const description = filteredMailLines.slice(descriptionIndex, descriptionIndex + 4).join(" ")
 
@@ -52,17 +58,26 @@ function extractPimmSolutionsFormData() {
           cardTextAsString = cardText.slice(cardText.lastIndexOf("1 x") + 2).join("\n")
       }
 
-      const amountOfTableRows = tableRowCount
-
-      const deliveryDate = filteredMailLines.find(word => word.includes("afleverdatum:")).split(":")[1].match(/([0-9]+(-[0-9]+)+)/i)[0]
 
       const extractedData = {
         'fromEmail': fromEmail,
         'from': from,
         'orderDate': orderDate,
-        'subject': subject
+        'subject': subject,
+        'deliveryDate': deliveryDate,
+        'companyName': companyName,
+        'name': name,
+        'adress': adress,
+        'city': city,
+        'postalCode': postalCode,
+        'telNumber': telNumber,
+        'description': description,
+        'price': price,
+        'withCard': withCard,
+        'cardText': cardTextAsString,
+        'comments': ''
       }
-      console.log(cardTextAsString)
+      console.log(description)
   })
 }
 
