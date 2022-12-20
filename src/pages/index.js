42ffc3529cdcfee1dd190bcb3f7395b712d7281b
Hajo300 <89904094+Hajo300@users.noreply.github.com>
2022-12-20 13:04:09 +0100

    started on customeroverview page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import MainLayout from '../layout/MainLayout';
-import CreateUser from './admin/createUser';
+import CreateUser from './admin/createUser/createUser';
 import ChangeUser from './admin/changeUser/changeUser';
+import CustomerOverview from './admin/customerOverview/customerOverview';
 
 export default function Home() {
   return (
     <MainLayout>
-      <ChangeUser />
+      <CustomerOverview />
     </MainLayout>
   );
 }
