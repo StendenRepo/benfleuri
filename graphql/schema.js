11b11afe67ac00d8ad59d871b8820f59f39f1194
Cedric Smit <Cedrsmit@gmail.com>
2022-12-21 16:20:17 +0100

    Graphql schema added

--- graphql/schema.js
+++ graphql/schema.js
@@ -2,6 +2,35 @@ import { gql } from 'apollo-server-micro';
 
 export const typeDefs = gql`
   #graphql
+
+  scalar DateTime
+  scalar currency
+
+  type Customer {
+  id: Int
+  firsname: String
+  lastname: String
+  city: String
+  phone_number: String
+  email: String
+  postcal_code: String
+  street_name: String
+  house_number: String
+  order: [Order]
+}
+
+  type Reciever {
+    id: Int
+    name: String
+    isAdmin: Boolean
+    password: String
+    order: [Order]
+  }
+
+  type Query {
+    numberSix: Int! # Should always return the number 6 when queried
+  }
+
   type Order {
     id: Int
     customerId: Int
@@ -10,8 +39,33 @@ export const typeDefs = gql`
     productInfo: String
     message: String
     extraInfo: String
-    cardType: Int
-    includeDelivery: boolean
-    price: decimal
+    cardType: CardType
+    includeDelivery: Boolean
+    price: currency
+    dateOfDelivery: DateTime
+    PaymentMethod: PaymentMethod
+    customer: [Customer]
+    reciever: Reciever
+    employee: Customer
+  }
+
+  enum CardType {
+    NONE
+    BASIC_CARD
+    RIBBON
+    SPECIAL_CARD
+  }
+
+  enum OrderState {
+    OPEN
+    CLOSED
+    IN_PROGRESS
+    DELIVERED
+  }
+
+  enum PaymentMethod {
+    CASH
+    PIN
+    BY_INVOICE
   }
 `;
