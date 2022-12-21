0e4f7189e9e2943460a2c2fdf2665ac479fd5f6c
Cedric Smit <Cedrsmit@gmail.com>
2022-12-21 16:16:43 +0100

    Fixed playground not being able to find the server

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,19 +1,38 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { typeDefs } from '../../../graphql/schema';
-import resolver from '../../../graphql/resolver';
-import Cors from 'micro-cors';
+import resolver from '../../../graphql/resolvers';
+import cors from 'micro-cors';
 
-const cors = Cors();
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers: [resolver],
+});
 
 const startServer = apolloServer.start();
 
-export default cors(async function handler(req, res) {
+export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
   await startServer;
 
   await apolloServer.createHandler({
     path: '/api/graphql',
-  })(res, req);
-});
+  })(req, res);
+};
 
-export const config = {};
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
