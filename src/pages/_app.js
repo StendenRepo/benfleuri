253e008f1cf3e35e94bd4b73a142caedbb409fc7
Cedric Smit <Cedrsmit@gmail.com>
2022-12-22 02:26:44 +0100

    Added Apollo Client

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../libs/apollo-client';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return;
+  <ApolloProvider client={apolloClient}>
+    <Component {...pageProps} />
+  </ApolloProvider>;
 }
 
 export default MyApp;
