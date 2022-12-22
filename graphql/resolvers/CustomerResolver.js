15f7e79e293c6a6d11473ba4967c61384d0cfc13
Cedric Smit <Cedrsmit@gmail.com>
2022-12-22 05:06:18 +0100

    Removed unused resolvers

--- graphql/resolvers/CustomerResolver.js
+++
@@ -1,9 +0,0 @@
-import prisma from '../../src/libs/prisma';
-
-export const CustomerResolver = {
-  // Query ALL customers
-  Query: {
-    customers: async (_parent, _args, ctx) =>
-      await ctx.prisma.customer.findMany(),
-  },
-};
