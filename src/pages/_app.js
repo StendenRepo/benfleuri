767e9d7922b9a78515d6d9c0a236f4efe5c61d9c
Cedric Smit <cedsmit@gmail.com>
2022-12-23 19:17:57 +0100

    fixed early return

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,11 @@ import { ApolloProvider } from '@apollo/client';
 import apolloClient from '../libs/apollo-client';
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <ApolloProvider client={apolloClient}>
-    <Component {...pageProps} />
-  </ApolloProvider>;
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
