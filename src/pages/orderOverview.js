e857b22e8693938c36a408b5fd196b65ba71b8ad
Monique <89770769+Moniquesbg@users.noreply.github.com>
2023-01-11 12:38:14 +0100

    added try catch, converted data to json objects

--- src/pages/orderOverview.js
+++ src/pages/orderOverview.js
@@ -94,128 +94,147 @@ export default function OrderOverview() {
 }
 
 async function importWooCommerceOrder() {
-  // import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
-  const WooCommerceRestApi =
-    require('@woocommerce/woocommerce-rest-api').default;
+  try {
+    // import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api"; // Supports ESM
+    const WooCommerceRestApi =
+      require('@woocommerce/woocommerce-rest-api').default;
 
-  const WooCommerce = new WooCommerceRestApi({
-    url: 'https://benfleuri.nl/',
-    consumerKey: process.env.REACT_APP_API_KEY,
-    consumerSecret: process.env.REACT_APP_SECRET_API_KEY,
-    version: 'wc/v3',
-  });
+    const WooCommerce = new WooCommerceRestApi({
+      url: 'https://benfleuri.nl/',
+      consumerKey: process.env.REACT_APP_API_KEY,
+      consumerSecret: process.env.REACT_APP_SECRET_API_KEY,
+      version: 'wc/v3',
+    });
 
-  //Authentication rest api
-  const querystring = require('querystring');
+    //Authentication rest api
+    const querystring = require('querystring');
 
-  const store_url = 'https://benfleuri.nl/';
-  const endpoint = '/wc-auth/v1/authorize';
-  const params = {
-    app_name: 'BenFleuri',
-    scope: 'read',
-    user_id: 1,
-    return_url: 'http://localhost:3000/orderOverview',
-    callback_url: 'http://localhost:3000/addOrder',
-  };
-
-  const query_string = querystring.stringify(params).replace(/%20/g, '+');
-  console.log(store_url + endpoint + '?' + query_string);
-
-  //test order: 39527 or 39685
-  WooCommerce.get('orders/39685').then((response) => {
-    //the data from the customer who ordered
-    const first_name = response.data.billing.first_name;
-    const last_name = response.data.billing.last_name;
-    const company = response.data.billing.company;
-    const address = response.data.billing.address_1;
-    const city = response.data.billing.city;
-    const postcode = response.data.billing.postcode;
-    const country = response.data.billing.country;
-    const email = response.data.billing.email;
-    const phone = response.data.billing.phone;
-
-    const ordererData = {
-      first_name: first_name,
-      last_name: last_name,
-      company: company,
-      address: address,
-      city: city,
-      postcode: postcode,
-      country: country,
-      email: email,
-      telNumber: phone,
+    const store_url = 'https://benfleuri.nl/';
+    const endpoint = '/wc-auth/v1/authorize';
+    const params = {
+      app_name: 'BenFleuri',
+      scope: 'read',
+      user_id: 1,
+      return_url: 'http://localhost:3000/orderOverview',
+      callback_url: 'http://localhost:3000/addOrder',
     };
 
-    //The data where the order should be shipped to
-    const shippingFirstName = response.data.shipping.first_name;
-    const shippingLastName = response.data.shipping.last_name;
-    const shippingCompany = response.data.shipping.company;
-    const shippingAddress = response.data.shipping.address_1;
-    const shippingCity = response.data.shipping.city;
-    const shippingpPostcode = response.data.shipping.postcode;
-    const shippingCountry = response.data.shipping.country;
-    const shippingTelNumber = response.data.shipping.phone;
+    const query_string = querystring.stringify(params).replace(/%20/g, '+');
+    //console.log(store_url + endpoint + '?' + query_string);
 
-    const shippingData = {
-      first_name: shippingFirstName,
-      last_name: shippingLastName,
-      shippingcompany: shippingCompany,
-      address: shippingAddress,
-      city: shippingCity,
-      postcode: shippingpPostcode,
-      country: shippingCountry,
-      telNumber: shippingTelNumber,
-    };
+    //test order: 39527 or 39685
+    WooCommerce.get('orders/39527').then((response) => {
+      //convert orderer data to json objects
+      const jsonCustomer = JSON.stringify(response.data.billing);
+      const customerData = JSON.parse(jsonCustomer);
 
-    //The data of the order
-    const status = response.data.status;
-    const paymentMethod = response.data.payment_method_title;
-    const shippingCost = response.data.shipping_total;
-    const totalOrderPrice = response.data.total;
-    const datePaid = response.data.date_paid;
-    const product = response.data.line_items[0].name;
-    const productQuantity = response.data.line_items[0].quantity;
-    const deliveryDate = response.data.meta_data[2].value;
-    const cardText = response.data.line_items[0].meta_data[1].value[0].value;
-    const cardType =
-      response.data.line_items[0].meta_data[5].value[0]?.[0].element.rules;
+      //the data from the customer who ordered
+      const first_name = customerData.first_name;
+      const last_name = customerData.last_name;
+      const company = customerData.company;
+      const address = customerData.address_1;
+      const city = customerData.city;
+      const postcode = customerData.postcode;
+      const country = customerData.country;
+      const email = customerData.email;
+      const phone = customerData.phone;
 
-    //convert cardtype and mourning ribbon to json
-    const jsonCardType = JSON.stringify(cardType);
-    const typeCardData = JSON.parse(jsonCardType);
+      const ordererData = {
+        first_name: first_name,
+        last_name: last_name,
+        company: company,
+        address: address,
+        city: city,
+        postcode: postcode,
+        country: country,
+        email: email,
+        telNumber: phone,
+      };
 
-    //Card type data
-    const noCard = typeCardData['Ik wil geen kaartje toevoegen_0'];
-    const basicCard = typeCardData['Gratis kaartje_1'];
-    const specialCard = typeCardData['Speciaal wenskaartje_2'];
-    const ribbon = typeCardData['Speciaal wenslintje_3'];
+      //convert shipping data to json objects
+      const jsonShipping = JSON.stringify(response.data.shipping);
+      const shipping = JSON.parse(jsonShipping);
 
-    //Card type check
-    const noCardCheck = noCard == '1' ? true : false;
-    const basicCardCheck = basicCard == '1' ? true : false;
-    const specialCardCheck = specialCard == '1' ? true : false;
-    const ribbonCheck = ribbon == '1' ? true : false;
+      //The data where the order should be shipped to
+      const shippingFirstName = shipping.first_name;
+      const shippingLastName = shipping.last_name;
+      const shippingCompany = shipping.company;
+      const shippingAddress = shipping.address_1;
+      const shippingCity = shipping.city;
+      const shippingpPostcode = shipping.postcode;
+      const shippingCountry = shipping.country;
+      const shippingTelNumber = shipping.phone;
 
-    const productInfo = {
-      product: product,
-      productQuantity: productQuantity,
-      status: status,
-      datePaid: datePaid,
-      paymentMethod: paymentMethod,
-      shippingCost: shippingCost,
-      totalOrderPrice: totalOrderPrice,
-      deliveryDate: deliveryDate,
-      cardText: cardText,
-      noCard: noCardCheck,
-      basicCard: basicCardCheck,
-      specialCard: specialCardCheck,
-      ribbonCheck: ribbonCheck,
-    };
+      const shippingData = {
+        first_name: shippingFirstName,
+        last_name: shippingLastName,
+        shippingcompany: shippingCompany,
+        address: shippingAddress,
+        city: shippingCity,
+        postcode: shippingpPostcode,
+        country: shippingCountry,
+        telNumber: shippingTelNumber,
+      };
 
-    console.log(ordererData);
-    console.log(shippingData);
-    console.log(productInfo);
-  });
+      //convert data order to json object
+      const jsonOrder = JSON.stringify(response.data);
+      const order = JSON.parse(jsonOrder);
+      const jsonOrderLevel1 = JSON.stringify(response.data.line_items[0]);
+      const orderLevel1 = JSON.parse(jsonOrderLevel1);
+
+      //The data of the order
+      const status = order.status;
+      const paymentMethod = order.payment_method_title;
+      const shippingCost = order.shipping_total;
+      const totalOrderPrice = order.total;
+      const datePaid = order.date_paid;
+      const product = orderLevel1.name;
+      const productQuantity = orderLevel1.quantity;
+      const deliveryDate = order.meta_data[2].value;
+      const cardText = orderLevel1.meta_data[1].value[0].value;
+      const cardType = orderLevel1.meta_data[5].value[0]?.[0].element.rules;
+
+      //Card type data
+      const noCard = cardType['Ik wil geen kaartje toevoegen_0'];
+      const basicCard = cardType['Gratis kaartje_1'];
+      const specialCard = cardType['Speciaal wenskaartje_2'];
+      const ribbon = cardType['Speciaal wenslintje_3'];
+
+      //Card type check
+      const noCardCheck = noCard == '1' ? true : false;
+      const basicCardCheck = basicCard == '1' ? true : false;
+      const specialCardCheck = specialCard == '1' ? true : false;
+      const ribbonCheck = ribbon == '1' ? true : false;
+
+      const productInfo = {
+        product: product,
+        productQuantity: productQuantity,
+        status: status,
+        datePaid: datePaid,
+        paymentMethod: paymentMethod,
+        shippingCost: shippingCost,
+        totalOrderPrice: totalOrderPrice,
+        deliveryDate: deliveryDate,
+        cardText: cardText,
+        noCard: noCardCheck,
+        basicCard: basicCardCheck,
+        specialCard: specialCardCheck,
+        ribbonCheck: ribbonCheck,
+      };
+
+      const extractedData = {
+        ordererData,
+        shippingData,
+        productInfo,
+      };
+
+      return extractedData;
+      // console.log(extractedData);
+    });
+  } catch (error) {
+    // console.log(error)
+    return null;
+  }
 }
 
 importWooCommerceOrder();
