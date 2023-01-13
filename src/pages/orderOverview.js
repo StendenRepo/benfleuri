37fb42a343d41c4d1174efec6a62e7adbde5c0d7
Monique <89770769+Moniquesbg@users.noreply.github.com>
2023-01-13 19:52:31 +0100

    schoonheidsfoutjes

--- src/pages/orderOverview.js
+++ src/pages/orderOverview.js
@@ -1,15 +1,35 @@
 import MainLayout from '../layout/MainLayout';
-import Link from 'next/link'
-import {updateOrderTable, getOrderTableData, OrderTable, TableRow, GreenButton, WhiteButton, nextPage, previousPage} from '../components/OrderTable'
-import {ArrowLeftIcon, ArrowRightIcon} from '@heroicons/react/20/solid';
+import Link from 'next/link';
+import {
+  updateOrderTable,
+  getOrderTableData,
+  OrderTable,
+  TableRow,
+  GreenButton,
+  WhiteButton,
+  nextPage,
+  previousPage,
+} from '../components/OrderTable';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/20/solid';
 
 function Header() {
   return (
     <div
       className={`border-b pb-[4%] flex flex-col font-['Roboto'] px-[4%] pt-[2%]`}
     >
-      <div className={`font-['Roboto'] ml-[2%] mb-[1%]`}>
-        <a href={'/'}>Dashboard</a>
+      <div
+        className={`flex font-['Roboto'] ml-[1%] mb-[1%] w-[150px] justify-between`}
+      >
+        <Link
+          className={`mr-[50px]`}
+          href={'/'}
+        >
+          <ArrowLeftIcon
+            className="h-5 w-5 pr-[2%] inline-block"
+            aria-hidden="true"
+          />
+          Dashboard
+        </Link>
       </div>
       <div className={`flex flex-row`}>
         <div
@@ -18,15 +38,18 @@ function Header() {
           Bestellingen
         </div>
         <div className={`flex justify-end w-1/2 gap-x-4`}>
-          <GreenButton>Nieuwe Bestelling</GreenButton>
+          <GreenButton link="/addOrder">Nieuwe Bestelling</GreenButton>
 
-
-            <button onClick={async () => {
-                await importWooCommerceOrder();
-            }
-            } className={`text-sm h-full font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
-         font-['Roboto'] bg-white text-black hover:bg-black hover:text-white`} type="button">Importeer Bestelling
-            </button>
+          <button
+            onClick={async () => {
+              await importWooCommerceOrder();
+            }}
+            className={`text-sm h-full font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
+         font-['Roboto'] bg-white text-black hover:bg-black hover:text-white`}
+            type="button"
+          >
+            Importeer Bestelling
+          </button>
           <WhiteButton>Exporteer Bestelling</WhiteButton>
         </div>
       </div>
@@ -35,68 +58,87 @@ function Header() {
 }
 
 export async function getServerSideProps() {
-    return getOrderTableData();
+  return getOrderTableData();
 }
 
-export default function OrderOverview({findAllOrders, findAllCustomers}) {
-    let content = updateOrderTable({
-        startIndex: 0, findAllOrders, findAllCustomers,
-        pageLoad: true
-    })
+export default function OrderOverview({ findAllOrders, findAllCustomers }) {
+  let content = updateOrderTable({
+    startIndex: 0,
+    findAllOrders,
+    findAllCustomers,
+    pageLoad: true,
+  });
 
-    return (
-        <MainLayout>
-            <Header/>
-            <div className={`h-full flex flex-col items-center py-0 px-8`}>
-                <div className="mt-[3%] overflow-auto items-center justify-center w-[95%]">
-                    <div className={`font-['Roboto'] items-start text-2xl font-bold w-1/2 pb-4`}>
-                        Aantal orders op pagina:
-                        <select className="text-sm h-full font-bold border-[1px] border-black rounded ml-5 py-[8px] px-[20px]
-                            font-['Roboto'] bg-white text-black" id="orderCount"
-                                onChange={() => {
-                                    updateOrderTable({startIndex: 0, findAllOrders, findAllCustomers})
-                                }}>
-                            <option>5</option>
-                            <option>10</option>
-                            <option>20</option>
-                        </select>
-                    </div>
-                    <div className={"mb-3"}>
-                        <button id={"prevButton"}
-                                className={`text-sm h-full font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
-         font-['Roboto'] bg-white text-black hover:bg-black hover:text-white disabled:bg-gray-300`} type="button"
-                                onClick={() => {
-                                    previousPage({findAllOrders, findAllCustomers})
-                                }
-                                }>
-                            <ArrowLeftIcon
-                                className="h-5 w-5 "
-                                aria-hidden="true"
-                            />
-                        </button>
-                        <button id={"nextButton"} className={`text-sm h-full font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
+  return (
+    <MainLayout>
+      <Header />
+      <div className={`h-full flex flex-col items-center py-0 px-8`}>
+        <div className="mt-[3%] overflow-auto items-center justify-center w-[95%]">
+          <div
+            className={`font-['Roboto'] items-start text-2xl font-bold w-1/2 pb-4`}
+          >
+            Aantal orders op pagina:
+            <select
+              className="text-sm h-full font-bold border-[1px] border-black rounded ml-5 py-[8px] px-[20px]
+                            font-['Roboto'] bg-white text-black"
+              id="orderCount"
+              onChange={() => {
+                updateOrderTable({
+                  startIndex: 0,
+                  findAllOrders,
+                  findAllCustomers,
+                });
+              }}
+            >
+              <option>5</option>
+              <option>10</option>
+              <option>20</option>
+            </select>
+          </div>
+          <div className={'mb-3'}>
+            <button
+              id={'prevButton'}
+              className={`text-sm h-full font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
          font-['Roboto'] bg-white text-black hover:bg-black hover:text-white disabled:bg-gray-300`}
-                                type="button" onClick={() => {
-                            nextPage({findAllOrders, findAllCustomers})
-                        }
-                        }>
-                            <ArrowRightIcon
-                                className="h-5 w-5 "
-                                aria-hidden="true"
-                            />
-                        </button>
-
-                    </div>
-                    <div id="tableContainer">
-                        <OrderTable data={content} orders={findAllOrders} customers={findAllCustomers}></OrderTable>
-                    </div>
-                    <div className={"mt-20 w-full flex justify-end"}>
-                        <GreenButton>Route maken</GreenButton>
-                    </div>
-                </div>
-            </div>
-        </MainLayout>
-    );
+              type="button"
+              onClick={() => {
+                previousPage({ findAllOrders, findAllCustomers });
+              }}
+            >
+              <ArrowLeftIcon
+                className="h-5 w-5 "
+                aria-hidden="true"
+              />
+            </button>
+            <button
+              id={'nextButton'}
+              className={`text-sm h-full font-bold border-[1px] border-black rounded py-[8px] px-[20px] 
+         font-['Roboto'] bg-white text-black hover:bg-black hover:text-white disabled:bg-gray-300`}
+              type="button"
+              onClick={() => {
+                nextPage({ findAllOrders, findAllCustomers });
+              }}
+            >
+              <ArrowRightIcon
+                className="h-5 w-5 "
+                aria-hidden="true"
+              />
+            </button>
+          </div>
+          <div id="tableContainer">
+            <OrderTable
+              data={content}
+              orders={findAllOrders}
+              customers={findAllCustomers}
+            ></OrderTable>
+          </div>
+          <div className={'mt-20 w-full flex justify-end'}>
+            <GreenButton>Route maken</GreenButton>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
 async function importWooCommerceOrder() {
   try {
